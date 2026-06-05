456ba0c5a6895a39707c4020d44292bd389babf4
zeeshake <paxipakistan@gmail.com>
2026-06-05 09:06:34 +0500

    Add live monitoring advanced views

--- src/App.tsx
+++ src/App.tsx
@@ -26,6 +26,7 @@ import Recordings             from './pages/Recordings'
 import CallIntelligence       from './pages/CallIntelligence'
 import OpsCenter              from './pages/OpsCenter'
 import ProductionMonitoring   from './pages/ProductionMonitoring'
+import LiveMonitoringAdvanced from './pages/LiveMonitoringAdvanced'
 import AdvancedDialingAnalytics from './pages/AdvancedDialingAnalytics'
 import SupportDiagnostics     from './pages/SupportDiagnostics'
 import ProductionReview       from './pages/ProductionReview'
@@ -84,6 +85,7 @@ export default function App() {
             <Route path="/call-intelligence"  element={<RoleRoute roles={adminSupervisorRoles}><CallIntelligence /></RoleRoute>} />
             <Route path="/ops"                element={<RoleRoute roles={adminSupervisorRoles}><OpsCenter /></RoleRoute>}      />
             <Route path="/monitoring"         element={<RoleRoute roles={adminSupervisorRoles}><ProductionMonitoring /></RoleRoute>} />
+            <Route path="/live-monitoring-advanced" element={<RoleRoute roles={adminSupervisorRoles}><LiveMonitoringAdvanced /></RoleRoute>} />
             <Route path="/advanced-dialing"   element={<RoleRoute roles={adminSupervisorRoles}><AdvancedDialingAnalytics /></RoleRoute>} />
             <Route path="/support/diagnostics" element={<RoleRoute roles={adminSupervisorRoles}><SupportDiagnostics /></RoleRoute>} />
             <Route path="/production/review"   element={<RoleRoute roles={['ADMIN']}><ProductionReview /></RoleRoute>} />
